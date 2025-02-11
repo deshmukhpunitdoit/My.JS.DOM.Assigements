092727f855a1b7278b07938be6fe854532ec49f3
punit deshmukh <deshmukhpunit.doit@gmail.com>
2025-02-11 17:56:04 +0530

    first commit

---
+++ index(1.2).js
@@ -0,0 +1,14 @@
+let showRoomel = document.getElementById("showroom");
+
+function firstCar(){
+    showRoomel.src = "images/img-1.jpg";
+}
+function secondCar(){
+    showRoomel.src = "images/img-2.jpg";
+}
+function thirdCar(){
+    showRoomel.src = "images/img-3.jpg";
+}
+function fourthCar(){
+    showRoomel.src = "images/img-4.jpg";
+}
